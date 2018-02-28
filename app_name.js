9e6c0129053e118a3a254951985e3a054975acbb
ivan kyselov <kyselovivan@gmail.com>
2018-02-28 16:46:10 +0200

    changing port to render

--- app_name.js
+++ app_name.js
@@ -8,6 +8,6 @@ app.get("/", function(req, res){
   res.render("index.ejs");
 });
 
-app.listen(8081, function(){
+app.listen(process.env.PORT, process.env.IP , function(){
   console.log("Server running");
 })
